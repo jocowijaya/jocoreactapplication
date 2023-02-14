6b7915e23cc1ecf71ada8882c51249f8531fff28
jocowijaya <jocowijaya@gmail.com>
2023-02-15 00:41:17 +1030

    Create components, props & how to apply useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
+import Employee from './component/Employee';
+import { useState } from 'react';
 
-function App() {
+function App() 
+{
+  //let role= "CASUAL"; // assign value to role 
+  const showEmployees = true; // do the check fundction to display info
+  const [role, setRole] = useState('Casual'); // set the useState & parse the value 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Joco! Hellowwowoooww
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          {showEmployees ? 
+          ( 
+            <>
+
+                <input type='text' onChange=
+                  {(e) => 
+                    {
+                      console.log(e.target.value);
+                      //role = e.target.value;
+                      setRole(e.target.value); // set Role using useState
+                    }
+                  }
+                />
+
+                <Employee name='Joco' role='Intern' />
+                <Employee name='Dion' role='FT Staff' />
+                <Employee name='Abby'  role={role}/>
+            </>
+          ) :
+
+              (
+                <p>You cannot see the employees</p>
+              )  
+          }
+   
     </div>
   );
 }
