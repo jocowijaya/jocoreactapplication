ccf2304bad23a79296004699693671486b66d242
jocowijaya <jocowijaya@gmail.com>
2023-02-15 00:43:37 +1030

    Creatingcomponts, props, apply useState

---
+++ src/component/Employee.js
@@ -0,0 +1,17 @@
+function Employee(props)
+{
+        return (
+            <>
+                <h3>Employee {props.name}</h3>
+                <p>{props.role ? props.role : 'No Role'}</p> 
+                {props.role ? 
+                    (<p class="role"> {props.role}</p>) 
+                    :
+                    (<p class="norole"> NO ROLE</p>)
+                }
+            </>
+            
+        );
+}
+
+export default Employee
